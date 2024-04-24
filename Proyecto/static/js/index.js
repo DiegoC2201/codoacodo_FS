3545a518bd65a10d26ffdfb9bd78061b9ea24cd2
DiegoC2201 <diegoc2201@gmail.com>
2024-04-23 23:23:30 -0300

    ajusté el carrusel para que corra automático

--- Proyecto/static/js/index.js
+++ Proyecto/static/js/index.js
@@ -1,32 +1,36 @@
 const grande = document.querySelector('.grande');
 const punto = document.querySelectorAll('.punto');
 
-//Cuando hago CLICK en un punto quiero
-    // Saber la posición del punto
-    // Aplicar un transform translateX al grande
-    // Quitar la clase ACTIVO a todos los puntos
-    // Añadir la clase ACTIVO al punto seleccionado
 
-punto.forEach( (cadaPunto, i)=> {
-    punto[i].addEventListener('click', ()=>{
+let i = 0;
+let posicion = 0;
 
-        let posicion = i
-        //queremos que cuando la posiión sea 0, el transformX sea 0
-        //cuando la posició>n sea 1, el transformX sea -20%
-        //cuando la posició>n sea 2, el transformX sea -40%
-        //cuando la posició>n sea 3, el transformX sea -60%
-        //cuando la posició>n sea 4, el transformX sea -80%
-        //por lo tanto la operación debería ser posicion * -20%
-        let operacion = posicion * -20
+//queremos que cuando la posiión sea 0, el transformX sea 0
+//cuando la posició>n sea 1, el transformX sea -20%
+//cuando la posició>n sea 2, el transformX sea -40%
+//cuando la posició>n sea 3, el transformX sea -60%
+//cuando la posició>n sea 4, el transformX sea -80%
+//por lo tanto la operación debería ser posicion * -20%
 
-        //movemos el div grande
-        grande.style.transform = `translateX(${ operacion }%)`
-
-        //quito la clase activo a todos los puntos
-        punto.forEach( (cadaPunto, i)=>{
-            punto[i].classList.remove('activo')
-        })
-        // y solo se la agrego al punto en el que se hizo click
-        punto[i].classList.add('activo')
+setInterval(function(){
+    grande.style.transform = `translateX(${ posicion }%)`;
+            
+    //quito la clase activo a todos los puntos
+    punto.forEach( (cadaPunto, i)=>{
+    punto[i].classList.remove('activo')
     })
-})
+    // y solo se la agrego al punto que corresponde según donde vaya i
+    punto[i].classList.add('activo')
+
+    //cuando transcurre el tiempo incremento i en 1 y calculo la nueva posición
+    i++;
+    posicion = i * -20;
+    
+    //reinicio i cuando llego al final del carrusel
+    if(i>(punto.length-1)){
+        i=0
+        posicion = i * -20;
+    }
+    
+
+},2500)
